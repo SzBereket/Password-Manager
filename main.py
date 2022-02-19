695657ea47499765f2efc7735f65531d9326b4d2
Sabribereket <80854069+Sabribereket@users.noreply.github.com>
2022-02-19 18:38:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,12 +12,14 @@ mycol = mydb["Accounts"]
 # mycol.insert_one(mydict)
 print("          Welcome        ")
 print("***************************")
-print("What You Wanna Do?\n***************************\n1.See Your Accounts Informations\n2.Create New Account\n3.Add New Accounts\n4.Change Your Accounts Information\n5.Remove Your Accounts\n9.Leave The Program\n***************************")
+# print("What You Wanna Do?\n***************************\n1.See Your Accounts Informations\n2.Create New Account\n3.Add New Accounts\n4.Change Your Accounts Information\n5.Remove Your Accounts\n9.Leave The Program\n***************************")
 
 while True:
+    time.sleep(3)
+    print("\n\nWhat You Wanna Do?\n***************************\n1.See Your Accounts Informations\n2.Create New Account\n3.Add New Accounts\n4.Change Your Accounts Information\n5.Remove Your Accounts\n9.Leave The Program\n***************************")
     print("Select the transaction: ")
     selection1 = input()
-    if selection1 == 9:
+    if int(selection1) == 9:
         print("Have Good Day")
         break    
     elif int(selection1) ==1:
@@ -53,12 +55,54 @@ while True:
                 
                 mydict = { "ID": newid ,"Title": newtitle , "Nickname": newnickname1 , "Password": fake.password() }
                 mycol.insert_one(mydict)
+                print("Successful")
+        elif int(passchoice)==2:
+            newpassword = input("Write the your password: ")
+            savechoice1 = input("Do you want me to save this data?\n 1.Yes \n 2.No\n")
+            if int(savechoice1)== 1:
+                mydict = { "ID": newid ,"Title": newtitle , "Nickname": newnickname1 , "Password": newpassword }
+                mycol.insert_one(mydict)
+                print("Successful")
+            
     
     elif int(selection1) ==3:
-        print("are u sureeee ")
-    
+        list1 =[]
+        for x in mycol.find():
+            list1.append(x['ID'])
+        newid = int(max(list1))+1
+        print("Write the new account Title:")
+        newtitle = input()
+        print("Write the new account Nickname:")
+        newnickname1 = input()
+        newpassword = input("Write the your password:\n ")
+        mydict = { "ID": newid ,"Title": newtitle , "Nickname": newnickname1 , "Password": newpassword }
+        mycol.insert_one(mydict)
+        print("Successful")
+
     elif int(selection1) ==4:
-        print("are u sureeee ")
+        for x in mycol.find():
+                print(str(x['ID'])+" "+x['Title']+" "+x['Password'])
+        changedata = input("Select Number:\n ")
+        for a in mycol.find():
+            if a['ID'] == int(changedata):
+                c = input("What do you want to update\n1.Title\n2.Nickname\n3.Password\n:")
+                if int(c)==1:
+                    newtitle2=input("Write New Title:\n")
+                elif int(c)==2: 
+                    newnickname2 = input("Write New Nickname")
+                elif int(c) ==3:
+                    newpswrdchoice=input("1.Automatically generate password.\n2.Write manual password:\n")
+                    if int(newpswrdchoice)==1:
+                        fake = Faker()
+                        print(fake.password())
+                        time.sleep(5)
+                        savechoice2 = input("Do you want me to save this data?\n 1.Yes \n 2.No:\n")
+                        if int(savechoice2) == 1:
+                            print("hello world")##data değişecek
+                            myquery = {"ID" : int(changedata)}    
+                            newvalues = { "$set": { "address": "Canyon 123" } }
+                else:
+                    print("Wrong Number")
     
     elif int(selection1) ==5:
         print("are u sureeee ")
